1c77104d91a79b8bfbd967240c3d5ae3f09860f9
alex-h-k <kuan8@hotmail.com>
2019-02-19 15:13:27 +1300

    building search_bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { component } from "react";
+import Searchbar from "../container/Search_bar";
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Searchbar />
+      </div>
     );
   }
 }
